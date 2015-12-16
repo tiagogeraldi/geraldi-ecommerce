9428b392cb0b5249723fd634ab8a653342e4bad8
Tiago Andre Geraldi <virgulla@gmail.com>
2015-12-16 00:33:42 -0200

    Fix indent

--- db/migrate/20151209012859_add_fk_to_carts.rb
+++ db/migrate/20151209012859_add_fk_to_carts.rb
@@ -1,7 +1,7 @@
 class AddFkToCarts < ActiveRecord::Migration
   def change
-  	add_foreign_key :carts, :products
+    add_foreign_key :carts, :products
 
-  	add_index :carts, :product_id
+    add_index :carts, :product_id
   end
 end
