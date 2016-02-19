eaa459670c390678eb807b58eb7b199911373ebe
Tiago Andre Geraldi <virgulla@gmail.com>
2016-02-19 14:39:45 -0200

    Testing Admin Products

---
+++ spec/features/admin_products_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+RSpec.feature "managing products", type: feature do
+  include LoginHelper
+    
+  before do
+    login_admin
+  end
+
+  scenario "create, update and destroy a product" do
+    brand    = create(:brand)
+    category = create(:category)
+
+    click_link 'Product'
+    click_link 'New'
+
+    fill_in 'Name', with: 'Sneakers A'
+    fill_in 'Price', with: '200,00'
+    fill_in 'Category name', with: category.name
+    fill_in 'Brand name', with: brand.name
+    screenshot_and_save_page
+    click_button 'Save and Close'
+
+    expect(page).to have_content('Sneakers A')
+
+    first('.fa-pencil').click
+    fill_in 'Name', with: 'Sneakers B'
+    click_button 'Save and Close'
+
+    fill_in 'Keyword...', with: 'non-existing'
+    click_button 'Search'
+    expect(page).to_not have_content('Sneakers')
+
+    fill_in 'Keyword...', with: 'sneak'
+    click_button 'Search'
+    expect(page).to have_content 'Sneakers B'
+
+    first('.delete').click
+    expect(page).to_not have_content 'Sneakers B'
+  end
+end
