29af41825937bf6b34ed6c016f77cd1c36e868f5
Tiago Andre Geraldi <virgulla@gmail.com>
2016-02-24 16:18:19 -0300

    Customer: Add Edit Account feature

--- app/controllers/customer_registrations_controller.rb
+++ app/controllers/customer_registrations_controller.rb
@@ -1,12 +1,18 @@
 class CustomerRegistrationsController < Devise::RegistrationsController
-  before_filter :configure_permitted_parameters, :only => [:create]
+  before_filter :configure_permitted_parameters, only: [:create, :update]
 
   protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) do |u| 
-        u.permit(:user_id, :name, :email, :address, :city, :state, 
-                 :zip_code, :phone, :password, :password_confirmation)
-      end
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) do |u| 
+      u.permit(:user_id, :name, :email, :address, :city, :state, 
+               :zip_code, :phone, :password, :password_confirmation)
     end
+
+    devise_parameter_sanitizer.for(:account_update) do |u| 
+      u.permit(:user_id, :name, :email, :address, :city, :state, 
+               :zip_code, :phone, :password, :password_confirmation,
+               :current_password)
+    end
+  end
 end
