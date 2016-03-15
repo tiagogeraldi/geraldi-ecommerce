db1e78d5c3015dcfe2d6c1577ccc4e8d549374bd
Tiago Andre Geraldi <virgulla@gmail.com>
2016-03-15 11:20:22 -0300

    Fixing cart uniqueness validation

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -3,7 +3,7 @@ class Cart < ActiveRecord::Base
   belongs_to :user
 
   validates :product_id, :quantity, :session_id, presence: true
-  validates :product_id, uniqueness: :session_id
+  validates :product_id, uniqueness: { scope: :session_id }
 
   def to_s
     product_id
